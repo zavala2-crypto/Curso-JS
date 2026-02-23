f524307a3b0e51a10a9b232768267748c0c77ed8
Alberto Zavala <albertotomas701@gmail.com>
2026-02-23 14:20:39 -0800

    javascript

---
+++ javascript-logica de programacao/Exercicio2/index.js
@@ -0,0 +1,18 @@
+// // capturar o evento submit
+// const form = document.querySelector('.form');
+// form.addEventListener('submit', function(z){
+//     z.preventDefault();
+//    // resultado();
+
+// });
+// function resultado (){
+//    // var res = document.querySelector('.resultado')
+//     const data = new Date()
+//     console.log(data)
+//    // res.innerHTML += data
+// }
+var res = document.querySelector('.resultado')
+    const data = new Date()
+     console.log(data)
+ res.innerHTML = data
+    
