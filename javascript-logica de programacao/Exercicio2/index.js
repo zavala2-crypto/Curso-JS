1f406cbb25f29091d768d297c0d0b5ef57d989d0
Alberto Zavala <albertotomas701@gmail.com>
2026-02-23 21:03:42 -0800

    Update index.js

--- javascript-logica de programacao/Exercicio2/index.js
+++ javascript-logica de programacao/Exercicio2/index.js
@@ -6,14 +6,34 @@ const data = new Date()
  const ano = data.getFullYear()
  const houra = data.getHours()
  const min = data.getMinutes()
+
  switch (diaSemana) {
     case 0:
-        
+        console.log('Domingo')
         break;
- 
+    case 1:
+        res.innerHTML += 'Segunda-feira, ' +dia+ ' de ' +mes+ ' de ' +ano+ '<br>' +houra+ ':' +min
+        break;
+    case 2:
+        console.log('Terca-feira')
+        break;
+     case 3:
+        console.log('Quarta-feira')
+        break;
+    case 4:
+        console.log('Quinta-feira')
+        break;
+    case 5:
+        console.log('Sexta-feira')
+        break;
+    case 6:
+        console.log('Sabada')
+        break;
+        
     default:
+        console.log('Dia Invalido!')
         break;
  }
- res.innerHTML += diaSemana+ ', ' +dia+ ' de ' +mes+ ' de ' +ano+ '<br>' +houra+ ':' +min
+ 
 
     
