a7a49dbd041a315b8088b1a398515178873c82b2
Alberto Zavala <albertotomas701@gmail.com>
2026-02-25 23:56:13 -0800

    javascript

--- javascript-logica de programacao/Lista de tarefas/tarefas.js
+++ javascript-logica de programacao/Lista de tarefas/tarefas.js
@@ -1,3 +1,26 @@
 const inputTarefa = document.querySelector('.input-tarefa')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefa = document.querySelector('.tarefa')
+
+function criaLi(){
+    const li = document.createElement('li')
+    return li;
+}
+function limpaInput(){
+    inputTarefa =  '';
+    inputTarefa.focus();
+}
+ function criaBotaoApagar() {
+
+    const BotaoApagar = document.createElement('button')
+    BotaoApagar.innerText = 'Apagar'
+ }
+function criaTarefa(textoInput){
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefa.appendChild(li);
+}
+ btnTarefa.addEventListener('click', function(e){
+    if(!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value)
+ })
